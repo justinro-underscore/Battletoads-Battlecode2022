234711c13566c4599625ae798d08bef3b16ec397
justinro-underscore <jcroderman@gmail.com>
2022-01-03 20:01:18 -0800

    Adds template for rushing player

---
+++ src/battletoadsrusher/droidrunners/ArchonRunner.java
@@ -0,0 +1,29 @@
+package battletoadsrusher.droidrunners;
+
+import battlecode.common.*;
+import battletoadsrusher.Utils;
+
+public class ArchonRunner {
+
+    /**
+     * Run a single turn for an Archon.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    public static void run(RobotController rc) throws GameActionException {
+        // Pick a direction to build in.
+        Direction dir = Utils.directions[Utils.rng.nextInt(Utils.directions.length)];
+        if (Utils.rng.nextBoolean()) {
+            // Let's try to build a miner.
+            rc.setIndicatorString("Trying to build a miner");
+            if (rc.canBuildRobot(RobotType.MINER, dir)) {
+                rc.buildRobot(RobotType.MINER, dir);
+            }
+        } else {
+        // Let's try to build a soldier.
+        rc.setIndicatorString("Trying to build a soldier");
+            if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
+                rc.buildRobot(RobotType.SOLDIER, dir);
+            }
+        }
+    }
+}
