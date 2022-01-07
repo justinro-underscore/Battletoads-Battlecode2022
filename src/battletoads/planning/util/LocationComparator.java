855badd3b46cba83c14d86071bda29eb8975a9be
Varun Chadha <vchadha023@gmail.com>
2022-01-06 20:07:05 -0600

    update greedy search cost function

--- src/battletoads/planning/util/LocationComparator.java
+++ src/battletoads/planning/util/LocationComparator.java
@@ -2,11 +2,10 @@ package battletoads.planning.util;
 
 import java.util.Comparator;
 
-// TODO: update this to use better cost function (look at cool down calculation...)
 public class LocationComparator implements Comparator<Location> {
     public int compare( Location l1, Location l2 ) {
-        double l1Cost = l1.getDistance() + 0.5 / l1.getPassability();
-        double l2Cost = l2.getDistance() + 0.5 / l2.getPassability();
+        double l1Cost = l1.getDistance() + l1.getPassability() / 10;
+        double l2Cost = l2.getDistance() + l2.getPassability() / 10;
 
         return ( (int) ( l1Cost - l2Cost ) );
     }
